4431f5a42df4b1e8fc60ddab05dad7aa8af1b946
OwenS3881 <owenski08@gmail.com>
2024-06-20 16:28:45 -0400

    Temp removed nav

--- frontend/app/components/Nav.jsx
+++ frontend/app/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import styles from "@/app/styles/Nav.module.css";
+//import styles from "@/app/styles/Nav.module.css";
 
 import Link from "next/link";
 
@@ -8,20 +8,21 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <header className={styles.container}>
-      <div className={styles.logoContainer}>
-        <Link href={"/"}>
-          <h2>Owen Szymanski</h2>
-        </Link>
-      </div>
-      <div className={styles.linksContainer}>
-        <Link href={"/projects/"}>Projects</Link>
-        <Link href={"/coursework/"}>Coursework</Link>
-        <Link href={"/research/"}>Research</Link>
-        <Link href={"/photos/"}>Photos</Link>
-        <Link href={"/journal/"}>Journal</Link>
-      </div>
-    </header>
+    <div></div>
+    // <header className={styles.container}>
+    //   <div className={styles.logoContainer}>
+    //     <Link href={"/"}>
+    //       <h2>Owen Szymanski</h2>
+    //     </Link>
+    //   </div>
+    //   <div className={styles.linksContainer}>
+    //     <Link href={"/projects/"}>Projects</Link>
+    //     <Link href={"/coursework/"}>Coursework</Link>
+    //     <Link href={"/research/"}>Research</Link>
+    //     <Link href={"/photos/"}>Photos</Link>
+    //     <Link href={"/journal/"}>Journal</Link>
+    //   </div>
+    // </header>
   );
 };
 
