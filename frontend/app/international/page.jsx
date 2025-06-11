50a71e6ccd145904831ab59dd2a55ebfc5743dc7
OwenS3881 <owenski08@gmail.com>
2025-06-11 14:24:12 +0200

    Added text for study abroad and more images to gallery

--- frontend/app/international/page.jsx
+++ frontend/app/international/page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import styles from "@/app/styles/International.module.css";
 import PictureAndCaption from "@/app/components/PictureAndCaption";
+import Link from "next/link";
 
 export const metadata = {
     title: "Study Abroad",
@@ -18,15 +19,118 @@ const InternationalPage = () => {
             </h2>
 
             <div className={styles.captionsContainer}>
-                <PictureAndCaption imgSrc={"/abroad-pics/good-hope.jpg"} />
+                <PictureAndCaption
+                    imgSrc={"/abroad-pics/good-hope.jpg"}
+                    text={
+                        <p>
+                            During the Summer of 2025, I had the amazing
+                            opportunity to particpate in the UF in Cape Town:
+                            Software Engineering and UX Design Internship Abroad
+                            program. The program lasted 5 weeks and had us
+                            focusing on developing applications to assist local
+                            community organizations in the work that they are
+                            doing. Under the guidance of our amazing professor{" "}
+                            <Link
+                                href="https://www.cise.ufl.edu/thomas-sanethia/"
+                                target="_blank"
+                            >
+                                Dr. Sanethia Thomas
+                            </Link>
+                            , I learned so much about software engineering and
+                            was able to see my work from a global perspective.
+                        </p>
+                    }
+                />
+
                 <PictureAndCaption
                     imgSrc={"/abroad-pics/safe-cities-meeting.jpg"}
+                    text={
+                        <p>
+                            Shortly after arriving in Cape Town, we set out to
+                            meet with our various companies. My company,{" "}
+                            <Link
+                                href="https://safecitiessa.com/"
+                                target="_blank"
+                            >
+                                Safe Cities
+                            </Link>{" "}
+                            is a community driven organization with a variety of
+                            programs designed to uplift the members of their
+                            community through activites such as skills
+                            development, leadership workshops, and empowering
+                            women in the community. For my internship I was
+                            tasked to work with Safe Cities' permaculture
+                            program. They were incredibly grateful to have a
+                            team of interns willing to work with them and were
+                            very eager to meet us. In our first meeting, they
+                            gave told us all of their ideas for what they wanted
+                            in their permaculture app to help manage their
+                            program.
+                        </p>
+                    }
                 />
                 <PictureAndCaption
                     imgSrc={"/abroad-pics/safe-cities-farm.jpg"}
+                    text={
+                        <p>
+                            Safe Cities had some ambitious ideas. They wanted an
+                            app that would allow for communication and
+                            management of their program. This included an
+                            information storage system for Safe Cities to share
+                            the knowledge they have gained over the years, a
+                            marketplace for farmers to sell their crops and
+                            communicate with one another, a scheduler system so
+                            that farmers could keep track of their tasks and
+                            what needs to get done, and a full admin management
+                            dashboard so that they had full control over the
+                            platform. It was an ambitious list with only a
+                            limited amount of time for development but our team
+                            was determined to deliver the best product we could.
+                        </p>
+                    }
                 />
-                <PictureAndCaption imgSrc={"/abroad-pics/hs-curro.jpg"} />
-                <PictureAndCaption imgSrc={"/abroad-pics/paragliding.jpg"} />
+                <PictureAndCaption
+                    imgSrc={"/abroad-pics/mid-dev-pics.png"}
+                    text={
+                        <p>
+                            Once planning was over, we were able to start the
+                            development process. The team and I worked
+                            dilligently throughout the program, whether it was
+                            when we went into the office or during our down time
+                            in between our various cultural experiences and
+                            activities, we maximized the limited development
+                            time we had on the program. We worked closely with
+                            our professor and the Safe Cities team to deliver a
+                            quality product that would work for their unique
+                            situation. On this page, you can see some pictures
+                            taken during the development process of the app.
+                        </p>
+                    }
+                />
+                <PictureAndCaption
+                    text={<p>Make this section about the final presentation</p>}
+                />
+                <PictureAndCaption
+                    imgSrc={"/abroad-pics/paragliding.jpg"}
+                    text={
+                        <p>
+                            Overall, this program has been one of the most
+                            valuable experiences of my college journey so far. I
+                            was able to learn so much more about software
+                            engineering, myself, and the world in such a short
+                            amount of time. Whether it be through the endless
+                            adventures we went on, the deeply impactful
+                            historical and cultural experiences, or the
+                            countless personal interactions with the local
+                            people, I became forever changed by this program.
+                            Going forward, I plan to take the new knowledge and
+                            experiences that I have gained and incorporate it
+                            into everything I do from my programming, to my
+                            interactions with people, to my persepctive on the
+                            world.
+                        </p>
+                    }
+                />
             </div>
         </div>
     );
--- frontend/app/photos/page.jsx
+++ frontend/app/photos/page.jsx
@@ -10,47 +10,52 @@ import path from "path";
 import GalleryPicture from "../components/GalleryPicture";
 
 export const metadata = {
-  title: "Photos",
+    title: "Photos",
 };
 
-const captions = new Map([
-  [
-    "graduation.JPG",
-    "Me giving the Salutatorian speech at my High School graduation.",
-  ],
-  ["uf-honors.jpg", "UF Honors Class of 2028!"],
-  [
-    "ursp.png",
-    "UF's Center For Undergraduate Research which I will be actively involved in as part of the URSP.",
-  ],
-  ["honors-me.jpg", "Me standing outside the Honors Village on a tour of UF."],
-  [
-    "young-me.jpg",
-    "Me standing outside the Computer Science building when I was a kid.",
-  ],
+const namesAndCaptions = new Map([
+    [
+        "good-hope.jpg",
+        "Me at the Cape of Good Hope on my Summer 2025 Study Abroad in Cape Town, South Africa",
+    ],
+    [
+        "cure-presentation.jpg",
+        "My CURE team and I presenting our research at the Spring Undergraduate Research Symposium",
+    ],
+    ["hipergator.jpg", "Me taking a tour of UF's super computer HiPerGator"],
+    [
+        "honors-me.jpg",
+        "Me standing outside the Honors Village on a tour of UF.",
+    ],
+    [
+        "graduation.JPG",
+        "Me giving the Salutatorian speech at my High School graduation.",
+    ],
+    [
+        "young-me.jpg",
+        "Me standing outside the Computer Science building when I was a kid.",
+    ],
 ]);
 
 const PhotosPage = async () => {
-  //get all images from the "gallery" folder
-  const imageDirectory = path.join(process.cwd(), "/public/gallery");
-  const imageFilenames = await fs.readdir(imageDirectory);
-
-  return (
-    <div className={styles.container}>
-      <h1>Photo Gallery</h1>
-      <div className={styles.gallery}>
-        {imageFilenames.map((srcFile, i) => (
-          <GalleryPicture
-            key={i}
-            src={`/gallery/${srcFile}`}
-            width={250}
-            height={250}
-            caption={captions.get(srcFile)}
-          />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <h1>Photo Gallery</h1>
+            <div className={styles.gallery}>
+                {Array.from(namesAndCaptions.entries()).map(
+                    ([filename, caption], i) => (
+                        <GalleryPicture
+                            key={i}
+                            src={`/gallery/${filename}`}
+                            width={250}
+                            height={250}
+                            caption={caption}
+                        />
+                    )
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default PhotosPage;
